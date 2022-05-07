6f4a0c60ecadd894e9d048dd98b74afc266c475d
Роман Соколов <sevo260@gmail.com>
2022-05-07 12:50:08 +0300

    загрузка файлов в базу

--- file_loader/urls.py
+++ file_loader/urls.py
@@ -3,5 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.load_client_org),
+    path('load_client_org', views.load_client_org),
+    path('load_bills', views.load_bills),
+
 ]
